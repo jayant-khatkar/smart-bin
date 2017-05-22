8e48b14441e36ee53ee0e8fc915fcb9d3fe3f726
corno93 <ascornelio@gmail.com>
2017-05-22 15:32:12 +1000

    added line for using voltage converters

--- gpio_test.py
+++ gpio_test.py
@@ -9,18 +9,23 @@ import time
 print "HERE WEEE GOOO!"
 
 pin = 18
+pin_ref_volt = 23
+
 
 GPIO.setmode(GPIO.BCM) # set board mode to broad com
 GPIO.setup(pin, GPIO.OUT) # set up pin as output
 
+#below sets up a random 3.3V reference pin. needed to convert 3.3V to 5V
+GPIO.setup(pin_ref_volt, GPIO.OUT)
+GPIO.output(pin_ref_volt, GPIO.HIGH)
 try:	
 	while True:		
 		GPIO.output(pin,GPIO.HIGH) #turn on pin 18
 		print 'up'	
 		time.sleep(1) #delay for 1 sec
 		GPIO.output(pin,GPIO.LOW)#turn off pin 18
+		print 'down'
 		time.sleep(1)
-		print ('down')
 except KeyboardInterrupt:
 	GPIO.cleanup()
 	print '\nClean them pins'
