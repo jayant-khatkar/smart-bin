91495918c963379ad9355665356e650f66717f6a
jayant-khatkar <jayant.khatkar@gmail.com>
2017-05-27 17:17:09 +1000

    added gitignore, starting ball tracking

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ if num_devices == 0:
 serial = fn.getDeviceSerialNumber(0)
 device = fn.openDevice(serial, pipeline=pipeline)
 
-listener = SyncMultiFrameListener(FrameType.Color | FrameType.Depth)
+listener = SyncMultiFrameListener(FrameType.Color |  FrameType.Ir | FrameType.Depth)
 # FrameType.Color | FrameType.Ir | FrameType.Depth)
 
 # Register listeners
@@ -39,10 +39,11 @@ device.setIrAndDepthFrameListener(listener)
 device.start()
 
 # NOTE: must be called after device.start()
-# registration = Registration(device.getIrCameraParams(),device.getColorCameraParams())
+registration = Registration(device.getIrCameraParams(),
+                            device.getColorCameraParams())
 
-# undistorted = Frame(512, 424, 4)
-# registered = Frame(512, 424, 4)
+undistorted = Frame(512, 424, 4)
+registered = Frame(512, 424, 4)
 
 # Optional parameters for registration
 # set True if you need
@@ -50,7 +51,7 @@ need_bigdepth = False
 need_color_depth_map = False
 
 bigdepth = Frame(1920, 1082, 4) if need_bigdepth else None
-color_depth_map = np.zeros((424, 512),  np.int32).ravel() \
+color_depth_map = Frame(424, 512,4) \
     if need_color_depth_map else None
 
 #huncher = cv2.createBackgroundSubtractorMOG2()
@@ -58,7 +59,7 @@ start = True
 while True:
     frames = listener.waitForNewFrame()
 
-    #color = frames["color"].asarray()
+    color = frames["color"]
     #####ir = frames["ir"]
     depth= frames["depth"]
     dep_arr = depth.asarray()
@@ -66,28 +67,28 @@ while True:
         start=False
         last_depth= depth
         ldep_arr = last_depth.asarray()
-    # registration.apply(color, depth, undistorted, registered,
-#                       bigdepth=bigdepth,
-#                       color_depth_map=color_depth_map)
+    registration.apply(color, depth, undistorted, registered)
     #masked = huncher.apply(depth.asarray() / 4500.)
 
     diff = (ldep_arr-dep_arr)*(dep_arr>0)*(ldep_arr>0)
     diff = diff*(diff>300)
-    diff2 = cv2.boxFilter(diff, -1, (10,10), anchor=(-1,-1), normalize=False)
+    diff2 = cv2.boxFilter(diff, -1, (5,5), anchor=(-1,-1), normalize=False)
     max_pos = np.argmax(diff2)
     max_val = np.max(diff2)
     x_pos = max_pos%512
     y_pos = int(np.floor(max_pos/512))
     #draw circle
+
+    disp_image = registered.asarray(np.uint8)
     print(max_val)
-    if max_val>5000:
-        cv2.circle(diff, (x_pos,y_pos), 10, 255, thickness=3, lineType=8, shift=0)
+    if max_val>10000:
+        cv2.circle(disp_image, (x_pos,y_pos), 10, 255, thickness=3, lineType=8, shift=0)
     # cv2.imshow("ir", ir.asarray() / 65535.)
     #cv2.imshow("depth", depth / 4500.)
-    cv2.imshow("depth2", diff)
+    #cv2.imshow("depth2", diff)
     #cv2.imshow("color", cv2.resize(color.asarray(),
     #                               (int(1920 / 3), int(1080 / 3))))
-    # cv2.imshow("registered", registered.asarray(np.uint8))
+    cv2.imshow("registered", disp_image)
 
     # if need_bigdepth:
 #        cv2.imshow("bigdepth", cv2.resize(bigdepth.asarray(np.float32),
