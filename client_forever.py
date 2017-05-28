bedff16119744e60f07f8a88310f28f188c58b03
Alex Cornelio <Alex@sig.home>
2017-05-28 22:02:51 +1000

    client_forever script yew

---
+++ client_forever.py
@@ -0,0 +1,35 @@
+# attempt at making a client that will poll forever
+
+import socket
+import sys
+
+def read(port):
+	s = socket.socket()
+
+	
+	host = '192.168.2.9'
+	s.connect((host,port))
+
+	
+	try:
+		msg = s.recv(1024)
+	#	print (msg.decode('ascii'))
+		s.close()
+	except socket.error, msg:		
+		sys.stderr.write('error %s'%msg[1])
+		s.close()
+		print 'close'
+		sys.exit(2)
+	return msg
+
+if __name__ == '__main__':
+	port = 1025
+	while True:
+		print 'hey, checking TCP socket'
+		data = read(port)
+		port = port + 1
+		print 'i just read %s' % data
+		print 'port num is: %d' % port	
+	
+
+
