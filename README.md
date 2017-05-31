d2b43a22a5d7b3374a030b2aaf8f4de634f8e0e9
Alex Cornelio <ascornelio@gmail.com>
2017-05-31 17:05:37 +1000

    arduino branch initial commit

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 Software for Robotic bin which detects paper balls being thrown at it and moves to catch them.
 
+
+__THIS IS THE ARDUINO BRANCH!__
+
+you need the sabertooth_simplifed library to run this code bra
---
+++ simplified_serial.ino
@@ -0,0 +1,24 @@
+
+#include <SabertoothSimplified.h>
+SabertoothSimplified ST; // We'll name the Sabertooth object ST.
+                         // For how to configure the Sabertooth, see the DIP Switch Wizard for
+                         //   http://www.dimensionengineering.com/datasheets/SabertoothDIPWizard/start.htm
+                         // Be sure to select Simplified Serial Mode for use with this library.
+                         // This sample uses a baud rate of 9600.
+                         //
+                         // Connections to make:
+                         //   Arduino TX->1  ->  Sabertooth S1
+                         //   Arduino GND    ->  Sabertooth 0V
+                         //   Arduino VIN    ->  Sabertooth 5V (OPTIONAL, if you want the Sabertooth to power the Arduino)
+                         //
+                         // If you want to use a pin other than TX->1, see the SoftwareSerial example.
+                                        
+void setup()
+{
+  SabertoothTXPinSerial.begin(9600); // This is the baud rate you chose with the DIP switches.
+}
+void loop()
+{
+  Serial.write(120);
+  delay(100);
+}
