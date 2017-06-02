c713bfa586af2aab52b9cfb6bf30edddea871391
Stephen <stephen.villanueva@hotmail.com>
2017-06-02 16:36:43 +1000

    BluetoothSplit

--- BluetoothServerModified/BluetoothServerModified.ino
+++ BluetoothServerModified/BluetoothServerModified.ino
@@ -1,17 +1,15 @@
-
-
-
-
-
 #include "SoftwareSerial.h"
 SoftwareSerial serial_connection(10, 11);//Create a serial connection with TX and RX on these pins
 
 
 #define BUFFER_SIZE 64//This will prevent buffer overruns.
-char inData[BUFFER_SIZE];//This is a character buffer where the data sent by the python script will go.
+char theta[4];//This is a character buffer where the data sent by the python script will go.
+char dist[3];//This is a character buffer where the data sent by the python script will go.
+char databuff[6];
 char inChar=-1;//Initialise the first character as nothing
 int count=0;//This is the number of lines sent in from the python script
 int i=0;//Arduinos are not the most capable chips in the world so I just create the looping variable once
+int idx = 1;
 
 void setup()
 {
@@ -24,48 +22,58 @@ void loop()
 {
 
   
-  //This will prevent bufferoverrun errors
+//This will prevent bufferoverrun errors
 //  byte byte_count=serial_connection.available();//This gets the number of bytes that were sent by the python script
   if(serial_connection.available())//If there are any bytes then deal with them
   {
+  //  Serial.println("we got data");
     byte byte_count = serial_connection.available();
-    
+    int distVal;
+    int thetaVal;
+
+
+    // Put the data into the array
      for(i=0;i<byte_count;i++)//Handle the number of incoming bytes
     {
       inChar=serial_connection.read();//Read one byte
-      inData[i]=inChar;//Put it into a character string(array)
-      Serial.println(inChar);
+      databuff[i] = inChar;
+//      Serial.println(inChar);
+      
+////      Serial.println("char val is " + String(inChar));
+//      if (i <= 2)
+//      {
+//       dist[i] = inChar;
+//      }
+//      else if (i > 2)
+//      {
+//        theta[i - 3] = inChar;
+//      }
+      
     }
 
-   Serial.println(inData);//Print to the monitor what was detected
-    //serial_connection.println("Hello from Blue "+String(count));//Then send an incrmented string back to the python script
+//    Serial.println("Hunch value is " + String(databuff));
+//    Serial.println("distance value is " + String(dist));
+//    Serial.println("theta value is " + String(theta));
 
-    inData[BUFFER_SIZE] = {0};
-    
+      int dist_hund = databuff[0] - '0'; 
+      int dist_tens = databuff[1] - '0'; 
+      int dist_ones = databuff[2] - '0'; 
+      int theta_hund = databuff[3] - '0'; 
+      int theta_tens = databuff[4] - '0'; 
+      int theta_ones = databuff[5] - '0'; 
 
-//    unsigned int part1;
-//    unsigned int part2;
-//
-//    Serial.println("now we gonna split");
-//    long int n;
-//    n = 123456;
-//    part1 = n / 1000;
-//    part2 = n - (part1 * 1000);
-//    Serial.println(part1);
-//    Serial.println(part2);
-    
+      distVal =   dist_hund*100 + dist_tens*10 + dist_ones;
+      thetaVal =  theta_hund*100 + theta_tens*10 + theta_ones;
+
+      Serial.print(distVal);
+      Serial.print(thetaVal);
+
+
+
   }
 
-  
-  
  delay(100);//Pause for a moment 
 
-
-
-
-
-
-  
 }
 
 
